ca03c30482e73a0361a55ae2f7d1c84089cae576
Cash Costello <cash.costello@gmail.com>
2010-10-23 17:21:00 -0400

    flushing caches now - still requires a refresh

--- actions/flush.php
+++ actions/flush.php
@@ -1,4 +1,29 @@
 <?php
 
+// elgg_invalidate_simplecache was added in 1.7.4
+if (!function_exists('elgg_invalidate_simplecache'))  {
+	function elgg_invalidate_simplecache() {
+		global $CONFIG;
+
+		$return = TRUE;
+
+		if ($handle = opendir($CONFIG->dataroot . 'views_simplecache')) {
+			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != "..") {
+					$return = $return && unlink($CONFIG->dataroot.'views_simplecache/'.$file);
+				}
+			}
+			closedir($handle);
+		} else {
+			$return = FALSE;
+		}
+
+		return $return;
+	}
+}
+
+elgg_invalidate_simplecache();
+elgg_filepath_cache_reset();
+
 system_message(elgg_echo('flush_cache:flushed'));
 forward(REFERER);
